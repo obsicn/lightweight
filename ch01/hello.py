ceca54f7a6a41a1c658571a408171c709783b0a3
obsicn <obsicn@hotmail.com>
2017-04-23 22:57:19 +0800

    modify for defining the wsgi application

--- ch01/hello.py
+++ ch01/hello.py
@@ -4,6 +4,7 @@ from django.conf.urls import url
 from django.http import HttpResponse
 from django.conf import settings
 from datetime import datetime
+from django.core.wsgi import get_wsgi_application
 
 
 def index(request):
@@ -21,6 +22,9 @@ settings.configure(
     ROOT_URLCONF=__name__,
 )
 
+application = get_wsgi_application()
+
+print(application)
 
 if __name__ == "__main__":
     from django.core.management import execute_from_command_line
